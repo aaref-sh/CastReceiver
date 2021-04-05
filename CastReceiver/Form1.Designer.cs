9f18a37696f345f15c6bf0cbfbe76db76f622d26
aaref-sh <m.aaref-sh@hotmail.com>
2021-04-05 22:47:41 +0300

    make all screen as one image and update each time

--- CastReceiver/Form1.Designer.cs
+++ CastReceiver/Form1.Designer.cs
@@ -29,105 +29,38 @@ namespace CastReceiver
         /// </summary>
         private void InitializeComponent()
         {
-            this.p00 = new System.Windows.Forms.PictureBox();
-            this.p01 = new System.Windows.Forms.PictureBox();
-            this.p02 = new System.Windows.Forms.PictureBox();
-            this.p10 = new System.Windows.Forms.PictureBox();
-            this.p11 = new System.Windows.Forms.PictureBox();
-            this.p12 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.p00)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p01)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p02)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p10)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p11)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p12)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
             this.SuspendLayout();
             // 
-            // p00
+            // pb
             // 
-            this.p00.Location = new System.Drawing.Point(0, 0);
-            this.p00.Name = "p00";
-            this.p00.Size = new System.Drawing.Size(300, 250);
-            this.p00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.p00.TabIndex = 0;
-            this.p00.TabStop = false;
-            // 
-            // p01
-            // 
-            this.p01.Location = new System.Drawing.Point(300, 0);
-            this.p01.Name = "p01";
-            this.p01.Size = new System.Drawing.Size(300, 250);
-            this.p01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.p01.TabIndex = 0;
-            this.p01.TabStop = false;
-            // 
-            // p02
-            // 
-            this.p02.Location = new System.Drawing.Point(600, 0);
-            this.p02.Name = "p02";
-            this.p02.Size = new System.Drawing.Size(300, 250);
-            this.p02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.p02.TabIndex = 0;
-            this.p02.TabStop = false;
-            // 
-            // p10
-            // 
-            this.p10.Location = new System.Drawing.Point(0, 250);
-            this.p10.Name = "p10";
-            this.p10.Size = new System.Drawing.Size(300, 250);
-            this.p10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.p10.TabIndex = 0;
-            this.p10.TabStop = false;
-            // 
-            // p11
-            // 
-            this.p11.Location = new System.Drawing.Point(300, 250);
-            this.p11.Name = "p11";
-            this.p11.Size = new System.Drawing.Size(300, 250);
-            this.p11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.p11.TabIndex = 0;
-            this.p11.TabStop = false;
-            // 
-            // p12
-            // 
-            this.p12.Location = new System.Drawing.Point(600, 250);
-            this.p12.Name = "p12";
-            this.p12.Size = new System.Drawing.Size(300, 250);
-            this.p12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.p12.TabIndex = 0;
-            this.p12.TabStop = false;
+            this.pb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb.Image = ((System.Drawing.Image)(resources.GetObject("pb.Image")));
+            this.pb.Location = new System.Drawing.Point(0, 0);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(958, 564);
+            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb.TabIndex = 0;
+            this.pb.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(958, 564);
-            this.Controls.Add(this.p12);
-            this.Controls.Add(this.p11);
-            this.Controls.Add(this.p10);
-            this.Controls.Add(this.p02);
-            this.Controls.Add(this.p01);
-            this.Controls.Add(this.p00);
+            this.Controls.Add(this.pb);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.p00)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p01)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p02)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p10)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p11)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.p12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox p00;
-        private System.Windows.Forms.PictureBox p01;
-        private System.Windows.Forms.PictureBox p02;
-        private System.Windows.Forms.PictureBox p10;
-        private System.Windows.Forms.PictureBox p11;
-        private System.Windows.Forms.PictureBox p12;
+        private System.Windows.Forms.PictureBox pb;
     }
 }
 
